e0e007a73015b2f15e6871f5818cd44176b2802a
muffin521 <mswwow@gmail.com>
2020-11-20 22:05:14 -0800

    updated help command part idek anymore, lost count

--- cogs/help commands.py
+++ cogs/help commands.py
@@ -10,14 +10,12 @@ class hcommands(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-    
-
-
 
 
     @commands.command()
     async def help(self, ctx):
-        pages = 2
+        
+        #pages = 2
         cur_page = 1
         #message = await ctx.send(f"Page {cur_page}/{pages}:\n{contents[cur_page-1]}")
         embed1 = discord.Embed(
@@ -42,7 +40,19 @@ class hcommands(commands.Cog):
         embed2.add_field(name='LOOΠΔ 1/3', value=f'''```\nHeejin\nHyunjin```''')
         embed2.add_field(name='LOOΠΔ Odd Eye Circle', value=f'''```\nKim Lip\nChoerry```''')
         embed2.add_field(name='LOOΠΔ yyxy', value=f'''```\n \n ```''')
+
+        embed3 = discord.Embed(
+            title = 'COMMANDS (3)',
+            description = 'All commands start with the prefix [=]',
+            colour = discord.Color.from_rgb(198, 237, 154))
+        embed3.add_field(name='Stray Kids [1]', value=f'''```\nFelix\nsHyunjin\nBangchan\nLee Know```''')
+        embed3.add_field(name='Stray Kids [2]', value=f'''```\nChangbin\nHan\nJeongin\nSeungmin```''')
+        # embed3.add_field(name='1', value=f'''```\n \n \n \n ```''')
+        # embed3.add_field(name='2', value=f'''```\n \n \n \n ```''')
+        # embed3.add_field(name='3', value=f'''```\n \n \n \n ```''')
+        # embed3.add_field(name='4', value=f'''```\n \n \n \n ```''')
         # getting the message object for editing and reacting
+
 
         message = await ctx.send(embed=embed1)
 
@@ -53,26 +63,33 @@ class hcommands(commands.Cog):
             return user == ctx.author and str(reaction.emoji) in ["⬅️", "➡️"]
             # This makes sure nobody except the command sender can interact with the "menu"
 
+
+
         while True:
             try:
                 reaction, user = await self.client.wait_for("reaction_add", timeout=60, check=check)
                 # waiting for a reaction to be added - times out after x seconds, 60 in this
                 # example
 
-                if str(reaction.emoji) == "➡️" and cur_page != pages:
-                    cur_page += 1
-                    await message.edit(embed=embed2)
-                    await message.remove_reaction(reaction, user)
-
-                elif str(reaction.emoji) == "⬅️" and cur_page > 1:
-                    cur_page -= 1
-                    await message.edit(embed=embed1)
-                    await message.remove_reaction(reaction, user)
-
-                # elif str(reaction.emoji) == "➡️" and cur_page == 2:
-                #     cur_page += 1
-                #     await message.edit(embed=embed3)
-                #     await message.remove_reaction(reaction, user)
+                if cur_page == 1:
+                    if str(reaction.emoji) == "➡️":
+                        cur_page += 1
+                        await message.edit(embed=embed2)
+                        await message.remove_reaction(reaction, user)
+                elif cur_page == 2:
+                    if str(reaction.emoji) == "➡️":
+                        cur_page += 1
+                        await message.edit(embed=embed3)
+                        await message.remove_reaction(reaction, user)
+                    elif str(reaction.emoji) == "⬅️":
+                        cur_page -= 1
+                        await message.edit(embed=embed1)
+                        await message.remove_reaction(reaction, user)
+                elif cur_page == 3:
+                    if str(reaction.emoji) == "⬅️":
+                        cur_page -= 1
+                        await message.edit(embed=embed2)
+                        await message.remove_reaction(reaction, user)
 
                 else:
                     await message.remove_reaction(reaction, user)
@@ -83,47 +100,6 @@ class hcommands(commands.Cog):
                 await message.clear_reactions()
                 break
                 # ending the loop if user doesn't react after x seconds
-
-
-
-#     #?Help Command [basic]
-#     @commands.command()
-#     async def help(self, ctx):
-#         embed=discord.Embed(
-#             title = 'HELP',
-#             description = 'All commands start with the prefix [=]',
-#             colour = discord.Color.from_rgb(198, 237, 154))
-#         embed.add_field(name='Help Commands', value=f'''```
-# Iz*One
-# Blackpink
-# Mamamoo
-# Loona
-# Red Velvet
-# Others
-# ```''')
-#         embed.add_field(name='Help', value=f'''
-# DM <@!{488423352206229505}>
-# to get your
-# command added
-# or with 
-# questions! 
-# ''')
-#         embed.add_field(name='Thanks to:', value=f'''\n<@389897179701182465>''')
-#         await ctx.send(embed=embed)
-#         await ctx.message.delete()
-
-
-#     #?loona help command
-#     @commands.command(aliases = ['helplooπδ'])
-#     async def helploona(self, ctx):
-#         embed=discord.Embed(
-#             title = 'IZ*ONE',
-#             description = 'All commands start with the prefix [=]',
-#             colour = discord.Color.from_rgb(198, 237, 154))
-#         embed.add_field(name='1/3', value=f'''```\nHeejin\nHyunjin```''')
-#         embed.add_field(name='Odd Eye Circle', value=f'''``````''')
-#         await ctx.send(embed=embed)
-#         await ctx.message.delete()
 
 
 
