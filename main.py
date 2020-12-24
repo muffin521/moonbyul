ed212eb3684dbccf7cc3b6ad4a16bfd43c5f9bf0
muffin521 <mswwow@gmail.com>
2020-12-24 00:49:42 -0800

    does this work?

--- main.py
+++ main.py
@@ -44,50 +44,7 @@ from discord.ext import commands
 
 muffin = 488423352206229505
 
-#//prefix things
-# Checks 'prefix.json' for prefixes.
-def get_prefix(client, message):
-    with open("./json/prefix.json", "r") as f:
-        prefix = json.load(f)
-    return prefix[str(message.guild.id)]
-
-
-#this sets prefix to '='
 client = commands.Bot(command_prefix= "=")
-#takes out help command
-client.remove_command('help')
-#//im literally crying W H Y doesnt this work when hosted
-# Default prefix = '='
-# @client.event
-# async def on_guild_join(guild):
-#     with open("./json/prefix.json", "r") as f:
-#         prefix = json.load(f)
-#     prefix[str(guild.id)] = "="
-#     with open("./json/prefix.json", "w") as f:
-#         json.dump(prefix, f, indent = 4)
-
-# # Remove prefix on server leave.
-# @client.event
-# async def on_guild_remove(guild):
-#     with open("./json/prefix.json", "r") as f:
-#         prefix = json.load(f)
-#     prefix.pop(str(guild.id))
-#     with open("./json/prefix.json", "w") as f:
-#         json.dump(prefix, f, indent = 4)
-
-# # Change Prefix command.
-# @client.command(aliases = ['moonbyulprefix', 'changeprefix', 'byulprefix'])
-# @commands.has_permissions(manage_messages=True)
-# async def prefix(ctx, message):
-#     with open("./json/prefix.json", "r") as f:
-#         prefix = json.load(f)
-#     prefix[str(ctx.guild.id)] = f"{message}"
-#     with open("./json/prefix.json", "w") as f:
-#         json.dump(prefix, f, indent = 4)
-#     embed=discord.Embed(
-#         description = f"Changed the servers prefix to `{message}`",
-#         colour = discord.Color.from_rgb(59,214,198))
-#     await ctx.send(embed=embed)
 
 
 
@@ -99,7 +56,11 @@ async def on_ready():
     # await client.change_presence(activity=activity)
     print('I say Mama-Mamamoo')
 
+#this sets prefix to '='
 
+#takes out help command
+client.remove_command('help')
+#//im literally crying W H Y doesnt this work when hosted
 
 #//cogs
 # This loads the cogs.
@@ -150,3 +111,44 @@ async def on_message(message):
     await client.process_commands(message)
 
 client.run('NzcwNzUwNjM1ODUwODU4NTA2.X5iHdw.GfBE19DGGzfFhTu2_4qz0o0n_I0')
+
+#//prefix things
+# Checks 'prefix.json' for prefixes.
+# def get_prefix(client, message):
+#     with open("./json/prefix.json", "r") as f:
+#         prefix = json.load(f)
+#     return prefix[str(message.guild.id)]
+
+
+
+# Default prefix = '='
+# @client.event
+# async def on_guild_join(guild):
+#     with open("./json/prefix.json", "r") as f:
+#         prefix = json.load(f)
+#     prefix[str(guild.id)] = "="
+#     with open("./json/prefix.json", "w") as f:
+#         json.dump(prefix, f, indent = 4)
+
+# # Remove prefix on server leave.
+# @client.event
+# async def on_guild_remove(guild):
+#     with open("./json/prefix.json", "r") as f:
+#         prefix = json.load(f)
+#     prefix.pop(str(guild.id))
+#     with open("./json/prefix.json", "w") as f:
+#         json.dump(prefix, f, indent = 4)
+
+# # Change Prefix command.
+# @client.command(aliases = ['moonbyulprefix', 'changeprefix', 'byulprefix'])
+# @commands.has_permissions(manage_messages=True)
+# async def prefix(ctx, message):
+#     with open("./json/prefix.json", "r") as f:
+#         prefix = json.load(f)
+#     prefix[str(ctx.guild.id)] = f"{message}"
+#     with open("./json/prefix.json", "w") as f:
+#         json.dump(prefix, f, indent = 4)
+#     embed=discord.Embed(
+#         description = f"Changed the servers prefix to `{message}`",
+#         colour = discord.Color.from_rgb(59,214,198))
+#     await ctx.send(embed=embed)
