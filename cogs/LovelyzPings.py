4171ffda9c21ec013db209ba27501904697b4fa6
muffin521 <mswwow@gmail.com>
2021-01-07 20:13:21 -0800

    jisoo gifs

--- cogs/LovelyzPings.py
+++ cogs/LovelyzPings.py
@@ -97,7 +97,8 @@ class gamerPings(commands.Cog):
             "https://gfycat.com/untimelywarmheartedbug",
             "https://gfycat.com/completeinfantilealaskankleekai",
             "https://gfycat.com/grotesquemisguidedauk",
-            "https://gfycat.com/BogusSadCoqui"]
+            "https://gfycat.com/BogusSadCoqui",
+            "https://gfycat.com/PrestigiousGroundedBeardedcollie"]
 
         self.bot.babysoul_gif = ["https://tenor.com/view/lovelyz-kpop-%EB%9F%AC%EB%B8%94%EB%A6%AC%EC%A6%88-%EB%B2%A0%EC%9D%B4%EB%B9%84%EC%86%8C%EC%9A%B8-babysoul-gif-18331417",
             "https://tenor.com/view/kpop-lovelyz-%EB%9F%AC%EB%B8%94%EB%A6%AC%EC%A6%88-%EB%B2%A0%EC%9D%B4%EB%B9%84%EC%86%8C%EC%9A%B8-babysoul-gif-18062427",
