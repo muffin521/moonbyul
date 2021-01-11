c73b6e0ac28839edb0fc7f7e61911e2adc24cde6
muffin521 <mswwow@gmail.com>
2021-01-11 13:35:22 -0800

    updated todo list in main.py

--- main.py
+++ main.py
@@ -17,6 +17,8 @@
     #& clean help command
     #& make desc on help command a variable
     #& change "invite the bot" to "invite Moonbyul" (help command)
+    #& make kbotcom a global variable
+    #& Give S.E.S command a title for each MV (in variable)
 
     #~ SNSD
 
