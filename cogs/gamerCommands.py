a68a3c250d69309666bf098f7b97e9d6b8b4d25a
muffin521 <mswwow@gmail.com>
2021-01-20 23:18:18 -0800

    removed a stray jungkook from =food

--- cogs/gamerCommands.py
+++ cogs/gamerCommands.py
@@ -74,7 +74,6 @@ class gamerPings(commands.Cog):
             "https://gfycat.com/harmlesssecondappaloosa",
             "https://gfycat.com/evendismalamphibian",
             "https://gfycat.com/unequaledindelibleblackmamba",
-            "https://gfycat.com/lankyultimategemsbok",
             "https://gfycat.com/gloriousfilthylacewing",
             "https://tenor.com/view/chuu-loona-kim-jiwoo-loonatheworld-kpop-gif-gif-19449093",
             "https://tenor.com/view/wayv-nct-xiao-xiaojun-xiao-de-jun-gif-17708611",
