7a5ca401ce81780fea09972a4d0dda42ecb7099a
muffin521 <mswwow@gmail.com>
2021-01-27 21:33:18 -0800

    chuu heart logs

--- cogs/LoonaPings.py
+++ cogs/LoonaPings.py
@@ -947,7 +947,11 @@ class LoonaPings(commands.Cog):
             await ctx.message.delete()
 
     @commands.command()
-    async def chuu(self, ctx, heart="no"):
+    async def chuu(self, ctx, heart="normalloona"):
+        now = datetime.now()
+        channel = ctx.bot.get_channel(self.bot.logs)
+        current_time = now.strftime("%H:%M:%S")
+        await channel.send(f"`{current_time} | USED COMMAND [Chuu {heart}] | USER: {ctx.author.name} [{(ctx.author.id)} | GUILD: {ctx.guild.name} [{ctx.guild.id}]]`" )
         if heart == "heart":
             if ctx.guild.id == luminary and ctx.channel.id != kbotcom:
                 await ctx.send(content=f'Wrong channel <@!{ctx.author.id}>! Go to #bot-commands', delete_after=2)
