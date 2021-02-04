2ff0fd48e608f5fc4624664307b6a4f6b3ffe8cc
muffin521 <mswwow@gmail.com>
2021-02-03 22:53:18 -0800

    I FUCKED UP

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ import json
 import os
 from discord.ext import commands
 
-client = commands.Bot(command_prefix= '==')
+client = commands.Bot(command_prefix= '=')
 client.remove_command('help')
 
 muffin = 488423352206229505
