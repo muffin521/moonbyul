b2ce8bf91ef805b99accdaafa0300ea1eb1f99ab
muffin521 <mswwow@gmail.com>
2021-02-24 11:38:49 -0800

    GWSN into help command~

--- cogs/help commands.py
+++ cogs/help commands.py
@@ -126,8 +126,9 @@ class hcommands(commands.Cog):
         embed6.add_field(name='fromis_9 [2]', value=f'''```\nfromis_9 Chaekyoung\nfromis_9 Nakyung\nfromis_9 Jiwon```''')
         embed6.add_field(name='fromis_9 [3]', value=f'''```\nfromis_9 Seoyeon\nfromis_9 Jiheon\nfromis_9 Gyuri```''')
 
-        embed6.add_field(name='ASTRO', value=f'```\nASTRO Eunwoo\nASTRO MJ\n \n \n \n ```')
+        embed6.add_field(name='GWSN', value=f'```\nGWSN Seokyoung\nGWSN Seoryoung\nGWSN Soso\nGWSN Anne\nGWSN Minju\nGWSN Miya```')
         embed6.add_field(name='Apink', value=f'```\nApink Bomi\nApink Chorong\nApink Eunji\nApink Hayoung\nApink Naeun\nApink Namjoo```')
+        embed6.add_field(name='ASTRO', value=f'```\nASTRO Eunwoo\nASTRO MJ\n \n \n \n ```')
         embed6.add_field(name='I Love You', value=f'```\n♡ \n♡ ```')
         embed6.add_field(name='Support:', value=f'\nhttps://discord.gg/Ntk9Jp26yx', inline = False)
         
