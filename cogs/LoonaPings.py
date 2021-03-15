4249ae8523b1455de8127ec732beaeea9da40b76
muffin521 <mswwow@gmail.com>
2021-03-14 21:44:01 -0700

    red velvet section is finally empty....

--- cogs/LoonaPings.py
+++ cogs/LoonaPings.py
@@ -840,7 +840,8 @@ class LoonaPings(commands.Cog):
             "https://gfycat.com/angryfatherlydegus-chaeryeong-itzy",
             "https://tenor.com/view/jaehyun-nct-gif-18953406",
             "https://tenor.com/view/yoona-im-yoona-%EC%9C%A4%EC%95%84-snsd-girls-generation-gif-14170845",
-            "https://cdn.discordapp.com/attachments/815387335419494431/816923992043683840/image0.gif"]
+            "https://cdn.discordapp.com/attachments/815387335419494431/816923992043683840/image0.gif",
+            "https://thumbs.gfycat.com/GregariousSkinnyEelelephant-size_restricted.gif"]
 
         self.bot.loona_ot12_gif = ["https://data.whicdn.com/images/337947512/original.gif",
             "https://thumbs.gfycat.com/SardonicSimilarBat-size_restricted.gif",
